3fcdd29a3a985273727e899907a0744134e13aa7
Ｄａｖｉｄ <davidgj1994@gmail.com>
2017-08-27 11:53:53 +0200

    working version

--- server.py
+++ server.py
@@ -4,9 +4,18 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+##########################################
+# GAME REVIEWS
+##########################################
+
 @app.route('/games/<game>')
 def game_review(game):
   return jsonify(get_game_review(game))
+
+
+##########################################
+# LATEST GAMES REVIEWED
+##########################################
 
 @app.route('/latest-games-reviewed')
 @app.route('/latest-games-reviewed/<platform>')
@@ -17,10 +26,19 @@ def latest_games_reviewed(platform="all"):
   else:
     return jsonify(get_latest_games_reviewed(platform, limit=5))
 
+
+##########################################
+# RELEASES
+##########################################
 @app.route('/releases')
+@app.route('/releases/<year>/<month>')
 @app.route('/releases/<year>/<month>/<platform>')
 def releases(platform="all", year=datetime.now().year, month=datetime.now().month):
   return jsonify(get_releases(platform, year, month))
+
+##########################################
+# START FLASK
+##########################################
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
